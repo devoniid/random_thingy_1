01979f57892d191b93db9e6bab680257a732a1f9
DD994321 <devoniid2609@gmail.com>
2022-10-11 10:46:02 -0500

    init

--- website/__init__.py
+++ website/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 def create_app(__Iodah__):
     app = Flask(__Iodah__)
     app.config['SECRET_KEY'] = 'the secret key is this'
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
