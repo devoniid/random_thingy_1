2150f0fa19adf2824f03d8664c55594a1429c686
BM001416 <benjimejia68@gmail.com>
2022-10-11 10:46:09 -0500

    Adding home HTML-Ben Added initial files-Ben

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-def create_app(__Iodah__):
-    app = Flask(__Iodah__)
+def create_app():
+    app = Flask(__name__)
     app.config['SECRET_KEY'] = 'the secret key is this'
     return app
