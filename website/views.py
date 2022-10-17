c0808680cf99d93cd7d6c686f36aee827a313a3c
BM001416 <benjimejia68@gmail.com>
2022-10-17 14:06:49 -0500

    link buttons to subjects

--- website/views.py
+++ website/views.py
@@ -5,3 +5,16 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/math')
+def math():
+    return render_template("math.html")
+@views.route('/reading')
+def reading():
+    return render_template("reading.html")
+@views.route('/science')
+def science():
+    return render_template("science.html")
+@views.route('/social_studies')
+def social_studies():
+    return render_template("social_studies.html")
