1b2de276be443dc879b6366a40710c9f451e3fa3
DD994321 <devoniid2609@gmail.com>
2022-10-18 13:46:15 -0500

    fixed

--- website/__init__.py
+++ website/__init__.py
@@ -5,6 +5,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'hello_world'
 
     from .views import views
-    apps.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
 
     return app
